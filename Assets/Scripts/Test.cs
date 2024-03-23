a54e7dac4463f543f73c3de4bb8901e158f43f5f
NikitaUshanov <nikita.ushanov.y@gmail.com>
2024-03-23 14:49:45 +0300

    ushanov | change test.cs

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector2 newVector = new Vector2(0, 0);
     }
 
     // Update is called once per frame
